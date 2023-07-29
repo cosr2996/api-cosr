4ad6bfa12f7a15295368ffe83ffa5494f54eef7d
omar sanchez <cosr2000@gmail.com>
2023-07-29 00:31:49 -0600

    agregando admin

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ dotenv.config();
 connectDB();
 
 // //? configurar cors
-  const whiteList =[process.env.FRONTEND_URL]
+  const whiteList =[process.env.FRONTEND_URL,process.env.VITE_BACKEND_ADMIN]
   const corsOptions ={
    origin: function(origin,callback){
      if(whiteList.includes(origin)){
